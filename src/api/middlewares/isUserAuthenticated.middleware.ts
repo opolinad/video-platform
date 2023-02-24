15d76afb4a0317efbecb46d88035d24dd6ae8dea
Daniel Andrés López Sabogal <andreslop@gmail.com>
2023-02-24 11:03:01 -0500

    add authentication chek for routes that needed

---
+++ src/api/middlewares/isUserAuthenticated.middleware.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express'
+import * as jwt from 'jsonwebtoken';
+
+export const isUserAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization.replace('Bearer ','');
+    try {
+        jwt.verify(token, process.env.JWT_SECRET);
+        next();
+    } catch (error) {
+        res.status(403).json({error:'User not authenticated'});
+    }
+
+}
--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,4 +1,5 @@
 import express, { Router } from 'express';
+import { isUserAuthenticated } from '../middlewares/isUserAuthenticated.middleware';
 import authRoutes from './auth.routes';
 import userRoutes from './user.routes';
 import videoRoutes from './video.routes';
@@ -6,6 +7,7 @@ import videoRoutes from './video.routes';
 const router:Router = express.Router();
 
 router.use('/auth', authRoutes);
+router.all('*',isUserAuthenticated);
 router.use('/user', userRoutes);
 router.use('/video', videoRoutes);
 
