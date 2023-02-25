0dac5d0971e5dd2aa6bfe480186f6606a9165241
Daniel Andrés López Sabogal <andreslop@gmail.com>
2023-02-24 23:48:11 -0500

    add video details

--- Front/src/components/VideoDetails.tsx
+++ Front/src/components/VideoDetails.tsx
@@ -1,19 +1,34 @@
-import { Card, CardHeader } from '@mui/material';
-import React from 'react';
+import { Card, CardHeader, CardMedia } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { videoProps } from './VideoCard';
 
 
 const VideoDetails = () => {
+    const [videoInformation, setVideoInformation] = useState(null);
+    const { videoId } = useParams();
+
+    useEffect(() => {
+        fetch(`http://${process.env.REACT_APP_API_URL}/video/${videoId}`)
+            .then(response => response.json())
+            .then(response => {
+                setVideoInformation(response.video);
+            });
+    }, []);
+
     return (
-        <>
-            <Card sx={{ margin: '0 3vw' }} >
-                <CardHeader
-                    title='Video Details'
-                    subheader='Here goes the title of the video'
-                />
+        <Card sx={{ margin: '0 3vw' }} >
+            {videoInformation && <CardHeader
+                title={(videoInformation as videoProps).title}
+                subheader={`${(videoInformation as videoProps).owner.firstName} ${(videoInformation as videoProps).owner.lastName}`}
+            />}
+            {videoInformation && <CardMedia
+                component='iframe'
+                height='800'
+                image={`${(videoInformation as videoProps).url}`}
+            />}
 
-            </Card>
-
-        </>
+        </Card>
     );
 }
 
